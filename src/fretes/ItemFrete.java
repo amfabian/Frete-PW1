29a927ecc582d195325f9a600e7f72a9f50067ca
Alexandre Fabian <xandy.fabian@gmail.com>
2021-11-15 18:55:17 -0300

    first commit

---
+++ src/fretes/ItemFrete.java
@@ -0,0 +1,35 @@
+package fretes;
+
+public class ItemFrete implements util.Validador {
+	private String descricao;
+	private Double peso;
+
+	public ItemFrete() {}
+	public ItemFrete(String descricao, Double peso) {
+		super();
+		this.descricao = descricao;
+		this.peso = peso;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Double getPeso() {
+		return peso;
+	}
+
+	public void setPeso(Double peso) {
+		this.peso = peso;
+	}
+
+	@Override
+	public String toString() {
+		return "ItemFrete [descricao=" + descricao + ", peso=" + peso + "]";
+	}
+
+}
---
+++ src/pessoas/Cliente.java
@@ -0,0 +1,49 @@
+package pessoas;
+
+public class Cliente extends Pessoa {
+	private String endereco;
+	private String telefone;
+	private String cpf;
+
+	public Cliente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public Cliente(String nome) {
+		super(nome);
+		// TODO Auto-generated constructor stub
+	}
+	public Cliente(String nome, String endereco, String telefone, String cpf) {
+		super(nome);
+		this.endereco = endereco;
+		this.telefone = telefone;
+		this.cpf = cpf;
+	}
+	public String getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+	public String getTelefone() {
+		return telefone;
+	}
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	@Override
+	public String toString() {
+		return "Cliente [" + super.toString() + ", Endereco: " + endereco + ", Telefone: " + telefone + ", CPF: " + cpf + "]\n";
+	} //arrumar para entregar o nome também.
+
+
+
+
+
+}
